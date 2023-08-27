562df1cf258783b82829c054b414095c429c81d0
Neha Choudhary <nehachoudhary1997@gmail.com>
2023-08-27 20:35:20 +0530

    new commit for variables

---
+++ TF_BASICS/variables/main.tf
@@ -0,0 +1,5 @@
+resource local_file file_new {
+  
+  filename             = var.filename11
+  sensitive_content    = var.content11
+}
---
+++ TF_BASICS/variables/variables.tf
@@ -0,0 +1,9 @@
+variable filename11 {
+    type=string
+    default="MySample.txt"
+}
+
+variable content11 {
+    type= string
+    default="Default Content placed in file"
+}
